fc8251aeb28d5ef81c7da4b3f5c6696176a021c6
Edu58 <edumuriithi58@gmail.com>
2022-03-19 11:31:27 +0300

    adds order constructor

---
+++ main.js
@@ -0,0 +1,9 @@
+// Order constructor
+function order (size, crust, topping, quantity, delivered, location) {
+    this.size = size
+    this.crust = crust
+    this.topping = topping
+    this.quantity = quantity
+    this.delivered = delivered
+    this.location = location
+}
