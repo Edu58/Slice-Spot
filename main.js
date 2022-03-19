428d00aecd228365fc92b87ea168da51f46fa180
Edu58 <edumuriithi58@gmail.com>
2022-03-19 12:25:30 +0300

    creates new order object

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
 $(function () {
 
+    // hide checkout card and location input
+    $('#order-card').hide()
+     $('#div-location').hide()
+    
+    // Toggle location input on clickiing delivered checkbox
+    $('#delivered').on('click', () => {
+        $('#div-location').toggle()
+    })
+
     // Order constructor
-    function order (size, crust, topping, quantity, delivered, location) {
+    function order (size, crust, topping, quantity, location) {
         this.size = size
         this.crust = crust
         this.topping = topping
         this.quantity = quantity
-        this.delivered = delivered
         this.location = location
     }
+
+    // Get form details
+    $('#form').on('submit', (e) => {
+        e.preventDefault()
+
+        const size = $('#size').val()
+        const crust = $('#crust').val()
+        const topping = $('#topping').val()
+        const quantity = $('#quantity').val()
+        const location = $('#location').val()
+
+        const Order = new order(size, crust, topping, quantity, location)
+        console.log(Order)
+    })
 });
